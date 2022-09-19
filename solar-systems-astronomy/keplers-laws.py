837e50f7c6ddf6c4fe7bb726856c3715b8a281c3
Ira Bell <ira@irabell.com>
2022-09-18 22:54:48 -0500

    Create keplers-laws.py

---
+++ solar-systems-astronomy/keplers-laws.py
@@ -0,0 +1,13 @@
+# find the AU with period, or period with AU
+period_1 = 4
+AU_1 = (period_1**2)**(1/3)
+print(AU_1)
+
+AU_2 = 6
+period_2 = (AU_2**3)**(.5)
+print(period_2)
+
+
+result = (2.5198420997897464/1.5874010519681994)
+print(result)
+
